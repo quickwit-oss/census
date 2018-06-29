dc3812cc260f269288bd5df574949137f5849f95
Paul Masurel <paul.masurel@gmail.com>
2018-06-29 13:13:25 +0900

    Added doc, ci and bumped version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Census
+
+This crate makes it possible to create an inventory object that keeps track of
+instances of a given type.
+
+It is used in tantivy to get an accurate list of all of the files that are still in use
+by the index, and avoid garbage collecting them.
+
+
+This `TrackedObject<T>` instance include some reference counting logic to ensure that
+the object is removed from the inventory once the last instance is dropped.
+
+
+# Example
+
+```rust
+
+
+
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
 //! ```rust
 //! extern crate census;
-//! use census::Inventory;
+//! use census::{Inventory, TrackedObject};
 //!
 //! fn main() {
-//!     let census = Inventory::new();
-//!     let value_1 = census.track(1);
+//!
+//!     let inventory = Inventory::new();
+//!
+//!     //  Each object tracked needs to be registered explicitely in the Inventory.
+//!     //  A `TrackedObject<T>` wrapper is then returned.
+//!     let one = inventory.track("one".to_string());
+//!     let two = inventory.track("two".to_string());
+//!
+//!     // A snapshot  of the list of living instances can be obtained...
+//!     // (no guarantee on their order)
+//!     let living_instances: Vec<TrackedObject<String>> = inventory.list();
+//!     assert_eq!(living_instances.len(), 2);
 //!
 //! }
 //! ```
@@ -13,6 +23,7 @@
 use std::sync::{Arc, RwLock};
 use std::ops::Deref;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::borrow::Borrow;
 
 
 /// The `Inventory` register and keeps track of all of the objects alive.
@@ -38,13 +49,71 @@ impl<T> Inventory<T> {
     }
 
     /// Takes a snapshot of the list of tracked object.
+    ///
+    /// Note that the list is a simple `Vec` of tracked object.
+    /// As a result, it is a consistent snapshot of the
+    /// list of living instance at the time of the call,
+    ///
+    /// Obviously, instances may have been created after the call.
+    /// They will obviously not appear in the snapshot.
+    ///
+    /// ```rust
+    /// # extern crate census;
+    /// # use census::{Inventory, TrackedObject};
+    /// # fn main() {
+    /// #
+    /// let inventory = Inventory::new();
+    ///
+    /// let one = inventory.track("one".to_string());
+    /// let living_instances: Vec<TrackedObject<String>> = inventory.list();
+    /// let two = inventory.track("two".to_string());
+    ///
+    /// // our snapshot is a bit old.
+    /// assert_eq!(living_instances.len(), 1);
+    ///
+    /// // a fresher snapshot would contain our new element.
+    /// assert_eq!(inventory.list().len(), 2);
+    /// # }
+    /// ```
+    ///
+    /// Also, the instance in the snapshot itself
+    /// are considered "living".
+    ///
+    /// As a result, as long as a snapshot is not dropped,
+    /// all of its instances will be part of the inventory.
+    ///
+    /// ```rust
+    /// # extern crate census;
+    /// # use census::{Inventory, TrackedObject};
+    /// # fn main() {
+    /// #
+    /// let inventory = Inventory::new();
+    ///
+    /// let one = inventory.track("one".to_string());
+    /// let living_instances: Vec<TrackedObject<String>> = inventory.list();
+    ///
+    /// // let's drop one here
+    /// drop(one);
+    ///
+    /// // The instance is technically still in the inventory
+    /// // as our previous snapshot is extending its life...
+    /// assert_eq!(inventory.list().len(), 1);
+    ///
+    /// // If we drop our previous snapshot however...
+    /// drop(living_instances);
+    ///
+    /// // `one` is really untracked.
+    /// assert!(inventory.list().is_empty());
+    /// # }
+    /// ```
+    ///
     pub fn list(&self) -> Vec<TrackedObject<T>> {
         self.items.read()
             .expect("Lock poisoned")
             .clone()
     }
 
-    /// Tracks a given `T` object
+    /// Starts tracking a given `T` object.
     pub fn track(&self, t: T) -> TrackedObject<T> {
         let self_clone: Inventory<T> = (*self).clone();
         let mut wlock = self.items
@@ -114,7 +183,17 @@ struct Inner<T> {
     idx: AtomicUsize,
 }
 
-/// A tracked object.
+/// Your tracked object.
+///
+///
+/// A tracked object contains reference counting logic and an
+/// `Arc` to your object.
+///
+/// It is cloneable but calling clone will not clone
+/// your internal object.
+///
+/// Your object cannot be mutated. You can borrow it using
+/// the `Deref` interface.
 pub struct TrackedObject<T> {
     census: Inventory<T>,
     inner: Arc<Inner<T>>,
@@ -131,6 +210,26 @@ impl<T> TrackedObject<T> {
     }
 
 
+    /// Creates a new object from an existing one.
+    ///
+    /// The new object will be registered
+    /// in your original object's inventory.
+    ///
+    /// ```rust
+    /// # extern crate census;
+    /// # use census::{Inventory, TrackedObject};
+    /// # fn main() {
+    /// #
+    /// let inventory = Inventory::new();
+    ///
+    /// let seven = inventory.track(7);
+    /// let fourteen = seven.map(|i| i * 2);
+    /// assert_eq!(*fourteen, 14);
+    ///
+    /// let living_instances = inventory.list();
+    /// assert_eq!(living_instances.len(), 2);
+    /// # }
+    /// ```
     pub fn map<F>(&self, f: F) -> TrackedObject<T>
         where F: FnOnce(&T)->T {
         let t = f(&*self);
@@ -142,6 +241,18 @@ impl<T> Deref for TrackedObject<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
+        &self.inner.val
+    }
+}
+
+impl<T> AsRef<T> for TrackedObject<T> {
+    fn as_ref(&self) -> &T {
+        &self.inner.val
+    }
+}
+
+impl<T> Borrow<T> for TrackedObject<T> {
+    fn borrow(&self) -> &T {
         &self.inner.val
     }
 }
@@ -206,6 +317,20 @@ mod tests {
                 .collect::<Vec<_>>(),
             vec![1]
         );
+    }
+
+    #[test]
+    fn test_census_list_extends_life() {
+        let census = Inventory::new();
+        let a = census.track(1);
+        let living = census.list();
+        assert_eq!(living.len(), 1);
+        drop(a);
+        let living_2 = census.list();
+        assert_eq!(living_2.len(), 1);
+        drop(living_2);
+        drop(living);
+        assert!(census.list().is_empty());
     }
 
     #[test]
